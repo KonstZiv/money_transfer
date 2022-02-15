b74682e07d37e0727ef484dae8dd7a81abe5e460
Konstyantin Zivenko <kos.zivenko@gmail.com>
2022-02-15 17:55:29 +0200

    processed with linters

--- app/src/main.py
+++ app/src/main.py
@@ -1,5 +1,3 @@
-# main.py
-
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -7,9 +5,15 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
+    """
+    test function 1 to check if it works
+    """
     return {"message": "Hello World"}
 
 
 @app.get("/items/{item_id}")
 async def read_item(item_id: int):
+    """
+    test function 2 to check if it works
+    """
     return {"item_id": item_id}
