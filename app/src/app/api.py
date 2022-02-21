3a95025aa89ce4ca138f55aa12c73c9f0440bcb0
Konstyantin Zivenko <kos.zivenko@gmail.com>
2022-02-21 09:09:37 +0100

    added describe for api-point in doc/about_task.md

--- app/src/app/api.py
+++ app/src/app/api.py
@@ -7,3 +7,21 @@ app = FastAPI()
 async def read_root() -> dict:
     return {"message": "This is root page"}
 
+@app.get('/user/{email_str}', tags=['user'])
+async def read_user(email_str) -> dict:
+    """
+    возвращает все данные по пользователю, включая состояние его счета
+    доступ: для пользователей автоизованных с ролью admin и operator - неограниченный
+    доступ: для авторизованного пользователя с ролью customer - только к своим данным
+    """
+    return {
+        'message': f'data for user: {email_str}'
+    }
+
+@app.post("/create_user/")
+async def create_user():
+    """
+    получив запрос создать в БД новго пользователя
+    """
+    pass
+
